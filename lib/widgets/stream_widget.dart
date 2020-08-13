42342f202ad416e1af4b3b01f5f37200c3415b87
Agustinus Theodorus <agustinus.theodorus@binus.edu>
2020-08-13 21:09:12 +0700

    Added new parameter to separator widget.

--- lib/widgets/stream_widget.dart
+++ lib/widgets/stream_widget.dart
@@ -11,28 +11,23 @@ Widget singleStream(String collectionName, {String orderByColumn = "", bool asce
   );
 }
 
-Widget listStream(String collectionName, {String orderByColumn = "", bool ascending = true}){
+Widget listStream(String collectionName, {String orderByColumn = "", bool ascending = true, bool separator = false}){
   return StreamBuilder(
     stream: orderByColumn == "" ? Firestore.instance.collection(collectionName).snapshots() : Firestore.instance.collection(collectionName).orderBy(orderByColumn, descending: true).snapshots(),
     builder: (context, snapshot){
       if(!snapshot.hasData) return new Center(child: new CircularProgressIndicator());
-      return ListView.builder(
-        itemCount: snapshot.data.documents.length,
-        itemBuilder: (context, index) => Container(),
-      );
-    }
-  );
-}
-
-Widget listSeparatorStream(String collectionName, {String orderByColumn = "", bool ascending = true}){
-  return StreamBuilder(
-    stream: orderByColumn == "" ? Firestore.instance.collection(collectionName).snapshots() : Firestore.instance.collection(collectionName).orderBy(orderByColumn, descending: true).snapshots(),
-    builder: (context, snapshot){
-      if(!snapshot.hasData) return new Center(child: new CircularProgressIndicator());
-      return ListView.builder(
-        itemCount: snapshot.data.documents.length,
-        itemBuilder: (context, index) => Container(),
-      );
+      if(!separator){
+        return ListView.builder(
+          itemCount: snapshot.data.documents.length,
+          itemBuilder: (context, index) => Container(),
+        );
+      }
+      else{
+        return ListView.builder(
+          itemCount: snapshot.data.documents.length,
+          itemBuilder: (context, index) => Container(),
+        );
+      }
     }
   );
 }
