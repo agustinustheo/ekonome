f8a16f38a8c8221389a9d5323859be56dd59a978
Eric Jonson <eric.jonson@binus.edu>
2020-08-21 01:23:17 +0700

    Fix: Modified choose_template page using group_checkbox

--- lib/pages/profile/choose_template.dart
+++ lib/pages/profile/choose_template.dart
@@ -1,11 +1,10 @@
 
 import 'package:EkonoMe/helpers/navigator_helper.dart';
-import 'package:EkonoMe/pages/home.dart';
 import 'package:EkonoMe/pages/profile/set_template.dart';
 import 'package:EkonoMe/widgets/background_widget.dart';
 import 'package:EkonoMe/widgets/button_widget.dart';
+import 'package:EkonoMe/widgets/checkbox_widget.dart';
 import 'package:EkonoMe/widgets/container_widget.dart';
-import 'package:EkonoMe/widgets/templateitem_widget.dart';
 import 'package:EkonoMe/widgets/title_widget.dart';
 import 'package:flutter/material.dart';
 
@@ -17,6 +16,11 @@ class ChooseTemplatePage extends StatefulWidget {
 
 class _ChooseTemplatePageState extends State<ChooseTemplatePage> {
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  final Map<int, String> checkboxValue = {
+    0: "40/30/20",
+    1: "50/40/10",
+    2: "30/30/20/20",
+  };
 
   @override
   Widget build(BuildContext context) {
@@ -36,9 +40,16 @@ class _ChooseTemplatePageState extends State<ChooseTemplatePage> {
                 child : subtitle('Choose or create your template')
               ),
               SizedBox(height: 30.0),
-              templateItem("40/30/20/10", context),
-              templateItem("30/30/20/20", context),
-              templateItem("50/30/20", context),
+              Row(
+                  children: <Widget>[
+                    Flexible(
+                      flex: 6,
+                      child : 
+                        CheckboxList(checkboxValue)
+                    ),
+                  
+                  ],
+                ),
               SizedBox(height: 40.0),
               fullButton(() => NavigatorHelper.pushReplacement(context, SetTemplatePage()), text: "Create your own template"),
               Center(
