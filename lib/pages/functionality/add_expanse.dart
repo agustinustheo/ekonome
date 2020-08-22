c284fdd50842df2fbbe67771084affa8edca2bc5
Agustinus Theodorus <agustinus.theodorus@binus.edu>
2020-08-22 11:36:54 +0700

    feat: connected add_expense view to firebase

--- lib/pages/functionality/add_expanse.dart
+++ lib/pages/functionality/add_expanse.dart
@@ -1,5 +1,6 @@
 import 'package:EkonoMe/helpers/firestore_helper.dart';
 import 'package:EkonoMe/helpers/session_helper.dart';
+import 'package:EkonoMe/widgets/alert_widget.dart';
 import 'package:EkonoMe/widgets/appbar_widget.dart';
 import 'package:EkonoMe/widgets/background_widget.dart';
 import 'package:EkonoMe/widgets/circularprogress_widget.dart';
@@ -19,9 +20,11 @@ class AddExpansePage extends StatefulWidget {
 }
 
 class _AddExpansePageState extends State<AddExpansePage> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
   Map<int, String> dropDownValues;
-  int _money;
-  String authUid;
+  List<dynamic> currMoney;
+  String _id, authUid, _profileId;
+  int _money, _savings;
 
   _AddExpansePageState() {
     SessionHelper.getUserLogin().then((value) async{
@@ -34,9 +37,24 @@ class _AddExpansePageState extends State<AddExpansePage> {
           }
         }
       );
+      
+      var profile = await FirestoreHelper.getFirestoreDocuments(
+        "profile", 
+        query: {
+          "=": {
+            "auth_uid": value
+          }
+        }
+      );
 
       setState(() {
         var data = q.documents[0]['titles'];
+        currMoney = q.documents[0]["funds"];
+        _id = q.documents[0].documentID;
+
+        _savings = profile.documents[0]['money'];
+        _profileId = profile.documents[0].documentID;
+        
         dropDownValues = new Map<int, String>();
         for(int i=0; i<data.length; i++){
           dropDownValues[i] = data[i];
@@ -50,30 +68,53 @@ class _AddExpansePageState extends State<AddExpansePage> {
     if(dropDownValues == null) return background(circularProgress());
     return background(
       container(
-        Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: <Widget>[
-            fullProfile(authUid),
-            Divider(
-              height: 10,
-              thickness: 2,
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            textField(
-              "Amount of money",
-              prefixIcon: Icon(Icons.account_balance_wallet),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            dropdownListAndButtonWidget(this.dropDownValues, () => {}),
-          ],
-        ),
+        Form(
+          key: _formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: <Widget>[
+              fullProfile(authUid),
+              Divider(
+                height: 10,
+                thickness: 2,
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              textField(
+                "Amount of money",
+                prefixIcon: Icon(Icons.account_balance_wallet),
+                onSaved: (input) => _money = int.parse(input)
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              dropdownListAndButtonWidget(this.dropDownValues, addToCategory),
+            ],
+          ),
+        )
       ),
       appBar: EkonomeAppBar("Add Expenses").getAppBar(),
     );
+  }
+
+  void addToCategory(int index) async{
+    final formState = _formKey.currentState;
+    if (formState.validate()) {
+      formState.save();
+      try {
+        int newSavings = _savings - _money;
+        if(newSavings < 0) throw ArgumentError("You do not have sufficient savings.");
+        currMoney[index] += _money;
+
+        FirestoreHelper.updateFirestore("templates", _id, {"funds": currMoney});
+        FirestoreHelper.updateFirestore("profile", _profileId, {"money": newSavings});
+        formState.reset();
+      } catch (ex) {
+        if(ex is ArgumentError) alertError(context, ex.message);
+        else alertError(context, "An exception occured");
+      }
+    }
   }
 }
