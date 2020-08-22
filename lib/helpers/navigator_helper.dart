70e286f3edecccb8fb05e162483c0df929295f60
Agustinus Theodorus <agustinus.theodorus@binus.edu>
2020-08-22 11:30:21 +0700

    feat: added popuntil function and route settings to all push methods.

--- lib/helpers/navigator_helper.dart
+++ lib/helpers/navigator_helper.dart
@@ -2,17 +2,20 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class NavigatorHelper{
-  static Future<dynamic> push(BuildContext context, Widget widget){
+  static Future<dynamic> push(BuildContext context, Widget widget, String route){
     return Navigator.of(context).push(
-      MaterialPageRoute(builder: (context) => widget)
+      MaterialPageRoute(settings: RouteSettings(name: route), builder: (context) => widget)
     );
   }
-  static Future<dynamic> pushReplacement(BuildContext context, Widget widget){
+  static Future<dynamic> pushReplacement(BuildContext context, Widget widget, String route){
     return Navigator.of(context).pushReplacement(
-      MaterialPageRoute(builder: (context) => widget)
+      MaterialPageRoute(settings: RouteSettings(name: route), builder: (context) => widget)
     );
   }
   static void pop(BuildContext context){
     return Navigator.pop(context);
+  }
+  static void popUntil(BuildContext context, String route){
+    return Navigator.of(context).popUntil(ModalRoute.withName(route));
   }
 }
