7fc112e6681cc9fbe5d96d8e8907e8afd3369110
Agustinus Theodorus <agustinus.theodorus@binus.edu>
2020-08-22 11:38:38 +0700

    feat: applied new chart stream builder.

--- lib/pages/home/home.dart
+++ lib/pages/home/home.dart
@@ -1,3 +1,5 @@
+import 'package:EkonoMe/helpers/firestore_helper.dart';
+import 'package:EkonoMe/helpers/navigator_helper.dart';
 import 'package:EkonoMe/helpers/session_helper.dart';
 import 'package:EkonoMe/models/ChartItemModel.dart';
 import 'package:EkonoMe/pages/functionality/add_expanse.dart';
@@ -9,6 +11,7 @@ import 'package:EkonoMe/widgets/chart_bar_widget.dart';
 import 'package:EkonoMe/widgets/circularprogress_widget.dart';
 import 'package:EkonoMe/widgets/container_widget.dart';
 import 'package:EkonoMe/widgets/profile_widget.dart';
+import 'package:EkonoMe/widgets/stream_widget.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -21,24 +24,12 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  List<ChartItemModel> charsData = [
-    ChartItemModel(0.0, "Investment", "Rp. 0/120.000"),
-    ChartItemModel(0.1, "Debt", "Rp. 0/120.000"),
-    ChartItemModel(0.2, "Angsuran", "Rp. 0/120.000"),
-    ChartItemModel(0.3, "Beli Kerupuk", "Rp. 0/120.000"),
-    ChartItemModel(0.4, "Makan Alpukat Bersama", "Rp. 0/120.000"),
-    ChartItemModel(0.5, "Beli Gelas", "Rp. 0/120.000"),
-    ChartItemModel(0.6, "Sewa Kamar", "Rp. 0/120.000"),
-    ChartItemModel(0.7, "Beli Dompet", "Rp. 0/120.000"),
-    ChartItemModel(0.8, "Jual Motor", "Rp. 0/120.000"),
-    ChartItemModel(0.9, "Hambur hamburin", "Rp. 0/120.000"),
-    ChartItemModel(1.0, "Pesta", "Rp. 0/120.000")
-  ];
+  List<ChartItemModel> charsData = new List<ChartItemModel>();
   String authUid = "";
 
   _HomePageState() {
-    SessionHelper.getUserLogin().then((value) async{
-      setState(() {
+    SessionHelper.getUserLogin().then((value){
+      setState((){
         authUid = value;
       });
     });
@@ -48,13 +39,6 @@ class _HomePageState extends State<HomePage> {
     return Container(
       decoration: BoxDecoration(
         color: Colors.white,
-        // boxShadow: [
-        //   BoxShadow(
-        //     color: Colors.black.withOpacity(0.1),
-        //     spreadRadius: 1,
-        //     blurRadius: 1,
-        //   ),
-        // ],
         border: Border.all(
           width: .5,
           color: Colors.green,
@@ -78,34 +62,20 @@ class _HomePageState extends State<HomePage> {
           children: <Widget>[
             fullProfile(authUid),
             this._buildContainer(
-              ListView.builder(
-                itemBuilder: (context, index) =>
-                    ChartBar(this.charsData[index]),
-                itemCount: this.charsData.length,
-              ),
+              chartStream({"=": {"auth_uid": authUid}})
             ),
-            SizedBox(
-              height: 20,
-            ),
+            SizedBox(height: 20),
             Column(
               children: <Widget>[
-                fullButton(() {
-                  Navigator.of(context).push(
-                      MaterialPageRoute(builder: (context) => AddFundPage()));
-                }, text: "Add Funds"),
-                SizedBox(
-                  height: 10,
-                ),
-                fullButton(() {
-                  Navigator.of(context).push(MaterialPageRoute(
-                      builder: (context) => AddExpansePage()));
-                }, text: "Add Expenses")
+                fullButton(() => NavigatorHelper.push(context, AddFundPage(), "Fund"), text: "Add Funds"),
+                SizedBox(height: 10),
+                fullButton(() => NavigatorHelper.push(context, AddExpansePage(), "Expense"), text: "Add Expenses")
               ],
             ),
           ],
         ),
       ),
-      appBar: EkonomeAppBar("EkonoMe").getAppBar(),
+      appBar: EkonomeAppBar("EkonoMe", context: context).getAppBar(),
     );
   }
 }
