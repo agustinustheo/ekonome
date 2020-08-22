be0b61dc49f00343eb3236f56a6a2e666c801686
Agustinus Theodorus <agustinus.theodorus@binus.edu>
2020-08-22 11:31:48 +0700

    feat: added edit profile, edit template, and logout buttons on appbar.

--- lib/widgets/appbar_widget.dart
+++ lib/widgets/appbar_widget.dart
@@ -1,14 +1,48 @@
+import 'package:EkonoMe/helpers/navigator_helper.dart';
+import 'package:EkonoMe/helpers/session_helper.dart';
+import 'package:EkonoMe/pages/auth/login.dart';
+import 'package:EkonoMe/pages/edit_profile/edit_choose_template.dart';
+import 'package:EkonoMe/pages/edit_profile/edit_profile.dart';
 import 'package:flutter/material.dart';
 
 class EkonomeAppBar {
   final String title;
+  final BuildContext context;
 
-  EkonomeAppBar(this.title);
+  EkonomeAppBar(this.title, {this.context});
 
   AppBar getAppBar() {
     return AppBar(
       title: Text(this.title),
       backgroundColor: Colors.greenAccent[200],
+      actions: [
+        context != null ? Container(
+          padding: EdgeInsets.only(right: 10.0),
+          child: Row(
+            children: [
+              IconButton(
+                icon: Icon(Icons.person),
+                onPressed: () async{
+                  NavigatorHelper.push(context, EditProfilePage(), "EditProfile");
+                }
+              ),
+              IconButton(
+                icon: Icon(Icons.list),
+                onPressed: () async{
+                  NavigatorHelper.push(context, EditChooseTemplatePage(), "EditChooseTemplate");
+                }
+              ),
+              IconButton(
+                icon: Icon(Icons.exit_to_app),
+                onPressed: () async{
+                  await SessionHelper.removeUserLogin();
+                  NavigatorHelper.pushReplacement(context, LoginPage(), "Login");
+                }
+              )
+            ],
+          )
+        ) : SizedBox()
+      ],
     );
   }
 }
